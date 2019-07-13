60a3251cdf1d1e427f4272bbde86a3b98f88d4c2
chaloo56 <chaloo56@gmail.com>
2019-07-14 00:50:45 +0300

    created component service and defined the typescript also made schanges to the service search-user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule, routingComponents } from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { SearchUserService } from './search-user.service';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SearchComponent } from './search/search.component';
+// import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    routingComponents,
-    PageNotFoundComponent
+    SearchComponent,
+    // routingComponents,
+    // PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
